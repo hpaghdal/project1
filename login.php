d6d2d82b19c0100f8843582addd698e0dc904e09
Hiren Paghdal <hdp36@njit.edu>
2018-10-01 15:53:16 -0400

    Changed title colors

--- login.php
+++ login.php
@@ -1,4 +1,16 @@
 <?php
+$user_name=$_POST ['username'];//Login Page
+$password=$_POST ['password'];//Login Page
+$firstname=$_POST ['firstname'];//registration page
+$lastname=$_POST ['lastname'];//registration page
+$email=$_POST ['email'];//registration page
+$DOB=$_POST ['DOB'];//registration page
+$password=$_POST ['password'];//registration page
+$questiontitle=$_POST ['questiontitle'];//Questions page
+$skills=$_POST ['skills'];//Questions page
+$questions=$_POST ['question'];//Questions page
+
+?>
 /**
  * Created by PhpStorm.
  * User: Hiren Paghdal
