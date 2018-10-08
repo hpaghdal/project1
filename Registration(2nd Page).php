a96198aae58377ea34f9d33b8c7a1f5ebb2b07ed
Hiren Paghdal <hdp36@njit.edu>
2018-10-08 15:09:21 -0400

    linked registration php page to html page. added the print out fields for the registration php page.

--- Registration(2nd Page).php
+++ Registration(2nd Page).php
@@ -1,1 +1,27 @@
 <?php
+///$user_name=$_POST ['email'];//Login Page
+///$password=$_POST ['password'];//Login Page
+$firstName=$_POST ['firstname'];//registration page
+$lastName=$_POST ['lastname'];//registration page
+$email=$_POST ['email'];//registration page
+$DOB=$_POST ['DOB'];//registration page
+$password=$_POST ['password'];//registration page
+///$questionTitle=$_POST ['questiontitle'];//Questions page
+///$skills=$_POST ['skills'];//Questions page
+///$question=$_POST ['question'];//Questions page
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+<body>
+<main>
+    First Name: <?php echo $firstName;?><br>
+    Last Name: <?php echo $lastName;?><br>
+    E-mail: <?php echo $email;?><br>
+    DOB: <?php echo $DOB;?><br>
+    Password: <?php echo $password;?><br>
+
+</main>
+</body>
+</html>
+</head>
