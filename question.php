d9ae25eafcf1e8a6ee40aae58c91ebe9882a60f8
Hiren Paghdal <hdp36@njit.edu>
2018-10-10 19:43:47 -0400

    added validation for question body input according to requirements.

--- question.php
+++ question.php
@@ -15,9 +15,45 @@ $question=$_POST ['question'];//Questions page
 <head>
 <body>
     <main>
-        Question title: <?php echo $questionTitle;?><br>
-        Skills: <?php echo array $skills;?><br>
-       Question Body: <?php echo $question;?><br>
+        <!--Question name-->
+        Question Title:
+        <?php
+        if ((strlen($questionTitle)==0)) {
+            echo("No question title entered");
+        } else if(strlen($questionTitle) <3) {
+            echo("Question title needs to be at least 3 characters");
+        } else {
+            echo($questionTitle);
+        }
+        ?><br>
+    <!--Skills-->
+        Skills:
+        <?php
+    if ((strlen($skills)==0)) {
+        echo("No skills entered");
+    } else {
+        echo($skills);
+    }
+        ?><br>
+
+        <!--Question Body-->
+        Question Body:
+        <?php
+        if ((strlen($question)==0)) {
+            echo("No question entered");
+         }   else if(strlen($question) >500) {
+                echo("Question body more then 500 characters, please shorten");
+        } else {
+            echo($question);
+        }
+        ?><br>
+
+
+
+
+
+
+
     </main>
 </body>
 </html>
