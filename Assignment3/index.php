88acd59e45db2c39b0c1f94f0106276b4f717872
Hiren Paghdal <hdp36@njit.edu>
2018-12-03 15:59:11 -0500

    added functions

--- Assignment3/index.php
+++ Assignment3/index.php
@@ -6,44 +6,49 @@ require('model/database.php');
 //require('model/ques.php');
 //require('model/registration.php');
 
+//CHECK THE VALUE IF ITS NULL
 $action = filter_input(INPUT_POST, 'action');
 if ($action == NULL) {
     $action = filter_input(INPUT_GET, 'action');
     if ($action == NULL) {
-        $action = 'showlog';
+        $action = 'loginForm';
     }
 }
-if ($action == 'showlog') {
-    echo "pagal";
-    include ('view/logForm.php');
-
+//SHOW LOGIN FORM
+if ( $action == 'loginForm'){
+    include('view/logForm.php');
 }
-else if ($action=="signin"){
-    echo "double pagal";
-    include ('model/log.php');
-
+//LOGIN ACTION
+else if ($action == 'signin') {
+    $email=$_POST ['email'];//Login Page
+    $password=$_POST ['password'];//Login Page
+    include('model/log.php');
+    $results = auth ($email, $password);
+    if ($valid && $results ){
+        header("Location:?action=QPage&&email=$email");
+    }
+    else
+        $error= "<br>Wrong username or password, please try again";
+    include("error.php");
 }
-else if ($action == 'showreg') {
-    echo "register";
-    include ('view/registrationForm.php');
+else if ( $action == 'showreg'){
+    include('view/registrationForm.php');
 }
 else if ($action == 'registration') {
-    $fname = filter_input(INPUT_POST, 'firstname', FILTER_SANITIZE_STRING);
-    $lname = filter_input(INPUT_POST, 'lastname', FILTER_SANITIZE_STRING);
-    $DOB = filter_input(INPUT_POST, 'DOB');
-    $email = filter_input( INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
-    $password = filter_input(INPUT_POST, 'password');
+    $fname=$_POST ['firstname'];//registration page
+    $lname=$_POST ['lastname'];//registration page
+    $email=$_POST ['email'];//registration page
+    $DOB=$_POST ['DOB'];//registration page
+    $password=$_POST ['password'];//registration page
     include('model/registration.php');
-    $results = userFromAccounts ($email);
-    if ($valid == true) {
-        if (count($results) > 0) {
-            echo " Email address is already registered<br><br>";
-            // IF EMAIL IS IN THE DATABASE GO BACK OR REGISTER OPTION
+    $results = getUserFromAccount($email);
+    if ($valid) {
+        if ($results) {
+            echo " this email address is already registered !! please use another email.<br>";
         }
         else {
-            addUser($email, $fname, $lname, $DOB, $password);
-            header("Location:?action=finalPage&&email=$email");
-//        redirect (" <h2>  $greetings $fname $lname, <br> <b style=\"color: green; text-align: center \"> Registration Successful !! Redirecting to Display Page..... </b> <h2>", "?action=finalPage&&email=$email'");
+            registration($email,$fname,$lname,$DOB,$password);
+            header("Location:?action=QPage&&email=$email");
         }
     }
     else {
@@ -52,11 +57,11 @@ else if ($action == 'registration') {
     }
 }
 //SHOW ADD QUESTION FORM
-else if ( $action == 'showAddQuestion'){
+else if ( $action == 'showNewQuestion'){
     $email = filter_input( INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
     include('model/QPage.php');
 }
-else if ($action == 'AddQuestion') {
+else if ($action == 'addQuestion') {
     $questionTitle = filter_input(INPUT_POST, 'questiontitle', FILTER_SANITIZE_STRING);
     $skills = filter_input(INPUT_POST, 'skills', FILTER_SANITIZE_STRING);
     $questionbody = filter_input(INPUT_POST, 'questionbody', FILTER_SANITIZE_STRING);
@@ -73,27 +78,27 @@ else if ($action == 'AddQuestion') {
         foreach ($results as $result) {
             $ownerid = $result['id'];
         }
-        addQuestion($email, $ownerid, $datetime, $questiontitle, $questionbody, $skills);
-//        header("Location: ?action=finalPage&&email=$email");
-        redirect(" Question Insert Successfully !! Redirecting to the Blog....... ","?action=finalPage&&email=$email");
+        addQuestion($email, $ownerid, $datetime, $questionTitle, $questionbody, $skills);
+        header("Location: ?action=model/QPage&&email=$email");
+
     }
     else{
         echo ' Try again';
     }
 }
-// EDIT QUESTIONS
-else if ($action == 'editQuestion') {
+// EDIT
+else if ($action == 'editQues') {
     $id = filter_input(INPUT_POST, 'id',FILTER_VALIDATE_INT);
     $dataFromQuestions = dataFromQuestionsById ($id);
     if ($id == NULL || $id == FALSE ) {
-        $error = "Missing or incorrect product id or category id.";
-        include('error/error.php');
+        $error = "Missing fields.";
+        include('error.php');
     } else {
         include('editQuestion.php');
     }
 }
-//UPDATE QUESTIONS
-else if ($action == 'updateQuestion') {
+//UPDATE
+else if ($action == 'updateQues') {
     $id = filter_input(INPUT_POST, 'id',FILTER_VALIDATE_INT);
     $qname = filter_input(INPUT_POST, 'questiontitle', FILTER_SANITIZE_STRING);
     $qskill = filter_input(INPUT_POST, 'skills', FILTER_SANITIZE_STRING);
@@ -105,37 +110,34 @@ else if ($action == 'updateQuestion') {
     $skills = implode(",", $skill_array);
     //Array Count
     $array_count = count($skill_array);
-    include('questionValidationCheck.php');
+    include('model/ques.php');
     if ($valid == true){
         updateQuestion($id, $qname, $qbody, $skills);
-        header("Location:?action=finalPage&&email=$email");
+        header("Location:?action=model/Qpage&&email=$email");
     }
     else{
         echo ' Try again';
     }
 }
-//DELETE QUESTIONS
-else if ($action == 'deleteQuestion') {
+//DELETE
+else if ($action == 'deleteQues') {
     $id = filter_input(INPUT_POST, 'id',FILTER_VALIDATE_INT);
     $email = filter_input( INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
     if ($id == NULL || $id == FALSE || $email == NULL || $email == FALSE ) {
-        $error = "Missing or incorrect product id or category id.";
-        include('error/error.php');
+        $error = "Missing fields";
+        include('error.php');
     } else {
         deleteQuestion($id);
-        header("Location: .?action=finalPage&&email=$email");
+        header("Location: .?action=model/Qpage&&email=$email");
     }
 }
-//FINAL DISPLAY PAGE/ QUESTION DISPLAY PAGE
-else if ($action =='finalPage'){
-    $greetings = greetings();
-    $email = filter_input( INPUT_GET, 'email', FILTER_SANITIZE_EMAIL);
-    $results = userFromAccounts ($email);
-    $dataFromQuestions = dataFromQuestions ($email);
-    include('finalPage.php');
+//QUESTION DISPLAY PAGE
+else if ($action =='QPage'){
+    //$greetings = greetings();
+    $email = $_GET['email'];
+    $results = questionDataByEmail($email);
+//    $dataFromQuestions = dataFromQuestions ($email);
+    include('model/Qpage.php');
 }
-// LOGOUT
-else if ( $action == 'logout'){
-    include('logout.php');
-}
+
 ?>
